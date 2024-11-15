9e715f55a74a27fd4bf2e4029f65bba4df692268
Wiktor Rzepka <144849665+MLDYX@users.noreply.github.com>
2024-11-15 20:42:59 +0100

    Add files via upload

--- Maps.cpp
+++ Maps.cpp
@@ -13,7 +13,7 @@ MapData loadMapaLato(RenderWindow& window)
 	mapData.backgroundTexture = window.loadTexture("Resources/tlo_lato.png");
 
 	float groundTileWidth = 30.0f;
-	int numGroundTiles = 160;
+	int numGroundTiles = 85;
 
 	for (int i = 0; i < numGroundTiles; ++i)
 	{
@@ -23,49 +23,46 @@ MapData loadMapaLato(RenderWindow& window)
 
 	mapData.obstacleSquare.push_back(GameObject(Vector2f(180, 170), obstacleSquareTexture));
 	mapData.obstacleSquare.push_back(GameObject(Vector2f(300, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(360, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(300, 170), obstacleSquareTexture));
 	mapData.obstacleSquare.push_back(GameObject(Vector2f(390, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420, 170), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(450, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(390, 140), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(450, 170), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(510, 110), obstacleSquareTexture));
 	mapData.obstacleSquare.push_back(GameObject(Vector2f(600, 110), obstacleSquareTexture));
 	mapData.obstacleSquare.push_back(GameObject(Vector2f(630, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(660, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(180 + 600, 170), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(300 + 600, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(360 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(390 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420 + 600, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420 + 600, 170), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(450 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(630 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(660 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(180 + 600 + 600, 170), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(300 + 600 + 600, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(360 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(390 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420 + 600 + 600, 140), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(420 + 600 + 600, 170), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(450 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(480 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(600 + 600 + 600, 110), obstacleSquareTexture));
-	mapData.obstacleSquare.push_back(GameObject(Vector2f(630 + 600 + 600, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(930, 140), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1050, 170), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1140, 140), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1230, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1260, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1380, 170), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1500, 140), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1560, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1590, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1620, 140), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1620, 170), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1650, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1680, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1800, 110), obstacleSquareTexture));
+	mapData.obstacleSquare.push_back(GameObject(Vector2f(1830, 110), obstacleSquareTexture));
 
 	mapData.obstacleTriangle.push_back(GameObject(Vector2f(240, 170.2), obstacleTriangleTexture));
 	mapData.obstacleTriangle.push_back(GameObject(Vector2f(270, 170.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(360, 110.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(450, 140.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(480, 140.2 - 0.4), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(780, 170.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(900, 140.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(1110, 140.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(1200, 110.2), obstacleTriangleTexture));
+	mapData.obstacleTriangle.push_back(GameObject(Vector2f(1290, 110.2), obstacleTriangleTexture));
 
 	mapData.stars.push_back(GameObject(Vector2f(180, 140), starTexture));
 	mapData.stars.push_back(GameObject(Vector2f(680, 140), starTexture));
 	mapData.stars.push_back(GameObject(Vector2f(1230, 80), starTexture));
 
-	mapData.finish.push_back(GameObject(Vector2f(4220, 80), finFinish));
-	mapData.finish.push_back(GameObject(Vector2f(4250, 80), ishFinish));
+	mapData.finish.push_back(GameObject(Vector2f(2100, 80), finFinish));
+	mapData.finish.push_back(GameObject(Vector2f(2130, 80), ishFinish));
 
 	return mapData;
 }
